f5410818a9f19bf1b57c9fdcfd1b2236ce7a4f79
Tom Maiaroto <tom.maiaroto@gmail.com>
2018-03-27 22:57:14 -0700

    update to use aegis

--- main.go
+++ main.go
@@ -1,35 +1,70 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/jasonmoo/lambda_proc"
 	"github.com/oschwald/geoip2-golang"
-	"log"
+	"github.com/fatih/structs"
+	aegis "github.com/tmaiaroto/aegis/framework"
 	"net"
+	"context"
+	"net/url"
+	"log"
+	"errors"
 )
 
+var db *geoip2.Reader
+
 func main() {
-	lambda_proc.Run(func(context *lambda_proc.Context, eventJSON json.RawMessage) (interface{}, error) {
-		var v map[string]interface{}
-		if err := json.Unmarshal(eventJSON, &v); err != nil {
-			return nil, err
+	// Load the database in main() in order to benefit from Lambda container re-use.
+	data, err := Asset("GeoLite2-City.mmdb")
+	if err == nil {
+		db, _ = geoip2.FromBytes(data)
+		defer db.Close()
+
+		// Handle API requests
+		router := aegis.NewRouter(fallThrough)
+		router.Handle("GET", "/", root)
+		// router.Listen()
+
+		// Handle RPCs
+		rpcRouter := aegis.NewRPCRouter()
+		rpcRouter.Handle("lookup", lookupProcedure)
+
+		handlers := aegis.Handlers{
+			Router:    router,
+			RPCRouter: rpcRouter,
 		}
-		return getLocation(v["source-ip"].(string))
-	})
+		handlers.Listen()
+	} else {
+		log.Println("Could not load GeoLite2-City.mmdb. Is it included in the binary?", err)
+	}
 }
 
-func getLocation(ip string) (*geoip2.City, error) {
-	db, err := geoip2.Open("GeoLite2-City.mmdb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+func fallThrough(ctx context.Context, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
+    res.StatusCode = 404
+    return nil
+}
 
-	parsedIp := net.ParseIP(ip)
-	record, err := db.City(parsedIp)
-	if err != nil {
-		log.Fatal(err)
-	}
+func root(ctx context.Context, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
+	record, err := lookup(evt.RequestContext.Identity.SourceIP)
+	res.JSON(200, record)
+    return err
+}
 
-	return record, err
+func lookupProcedure(ctx context.Context, evt *map[string]interface{}) (map[string]interface{}, error) {
+	var resp map[string]interface{}
+	if evt != nil {
+		derefEvt := *evt
+		record, err := lookup(derefEvt["ipAddress"].(string))
+		if err == nil {
+			resp = structs.Map(record)
+			return resp, err
+		}
+		return resp, err
+	}
+	return resp, errors.New("no IP address passed to procedure")
+}
+
+func lookup(ipAddress string) (*geoip2.City, error) {
+	parsedIP := net.ParseIP(ipAddress)
+	return db.City(parsedIP)
 }
