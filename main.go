5e863151db348d86614f4fa8a3005d231e6b467f
Tom Maiaroto <tom.maiaroto@gmail.com>
2018-04-10 23:34:49 -0700

    updated to use latest aegis

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/oschwald/geoip2-golang"
 	"github.com/fatih/structs"
 	aegis "github.com/tmaiaroto/aegis/framework"
+	"github.com/aws/aws-xray-sdk-go/xray"
 	"net"
 	"context"
 	"net/url"
@@ -14,11 +15,30 @@ import (
 var db *geoip2.Reader
 
 func main() {
+	// log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	// Load the database in main() in order to benefit from Lambda container re-use.
-	data, err := Asset("GeoLite2-City.mmdb")
+	var err error
+	var data []byte
+	
+	ctx, seg := xray.BeginSegment(context.Background(), "GeoipInit")
+	ctx, subSeg := xray.BeginSubsegment(ctx, "LoadDatabase")
+
+	err = xray.Capture(ctx, "LoadBytes", func(ctx1 context.Context) error {
+		data, err = Asset("GeoLite2-City.mmdb")
+		return err
+	})
+	
 	if err == nil {
-		db, _ = geoip2.FromBytes(data)
+		xray.Capture(ctx, "DatabaseFromBytes", func(ctx1 context.Context) error {
+			db, _ = geoip2.FromBytes(data)
+			return nil
+		})
 		defer db.Close()
+
+		// Close the segment and subsegment
+		subSeg.Close(nil)
+		seg.Close(nil)
 
 		// Handle API requests
 		router := aegis.NewRouter(fallThrough)
@@ -33,28 +53,28 @@ func main() {
 			Router:    router,
 			RPCRouter: rpcRouter,
 		}
+
 		handlers.Listen()
 	} else {
 		log.Println("Could not load GeoLite2-City.mmdb. Is it included in the binary?", err)
 	}
 }
 
-func fallThrough(ctx context.Context, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
+func fallThrough(ctx context.Context, d *aegis.HandlerDependencies, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
     res.StatusCode = 404
     return nil
 }
 
-func root(ctx context.Context, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
+func root(ctx context.Context, d *aegis.HandlerDependencies, evt *aegis.APIGatewayProxyRequest, res *aegis.APIGatewayProxyResponse, params url.Values) error {
 	record, err := lookup(evt.RequestContext.Identity.SourceIP)
 	res.JSON(200, record)
     return err
 }
 
-func lookupProcedure(ctx context.Context, evt *map[string]interface{}) (map[string]interface{}, error) {
+func lookupProcedure(ctx context.Context, d *aegis.HandlerDependencies, evt map[string]interface{}) (map[string]interface{}, error) {
 	var resp map[string]interface{}
 	if evt != nil {
-		derefEvt := *evt
-		record, err := lookup(derefEvt["ipAddress"].(string))
+		record, err := lookup(evt["ipAddress"].(string))
 		if err == nil {
 			resp = structs.Map(record)
 			return resp, err
